e6c2f59c04c2dac209de4937ec0bbf90df18e978
ftrbnd <giosalas25@gmail.com>
2024-07-10 16:33:22 -0700

    Hide header items when not signed in

---
+++ src/components/UserMenu.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from './ui/button';
+import Link from 'next/link';
+import { useAuth, useUser } from '@clerk/nextjs';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { CircleUser } from 'lucide-react';
+
+export function UserMenu() {
+	const { signOut } = useAuth();
+	const { user } = useUser();
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button
+					variant='secondary'
+					size='icon'
+					className='rounded-full'>
+					{user ? (
+						<Avatar>
+							<AvatarImage src={user.imageUrl} />
+							<AvatarFallback>
+								{user.firstName ? user.firstName[0] : ''}
+								{user.lastName ? user.lastName[0] : ''}
+							</AvatarFallback>
+						</Avatar>
+					) : (
+						<CircleUser className='h-5 w-5' />
+					)}
+
+					<span className='sr-only'>Toggle user menu</span>
+				</Button>
+			</DropdownMenuTrigger>
+			{user && (
+				<DropdownMenuContent align='end'>
+					<DropdownMenuLabel>My Account</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem asChild>
+						<Link href='/account'>Settings</Link>
+					</DropdownMenuItem>
+					<DropdownMenuItem onClick={() => signOut()}>
+						Sign out
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			)}
+		</DropdownMenu>
+	);
+}
