61e04fa557d3e19629391696dcadcd54c12096c0
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2024-07-18 17:39:05 -0700

    Fix height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ApartmentsTable } from '@/components/Apartment/ApartmentsTable';
 
 export default function Dashboard() {
 	return (
-		<div className='flex w-full flex-col'>
+		<div className='flex w-full flex-col min-h-screen'>
 			<main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 bg-muted/40'>
 				<ApartmentsTable />
 			</main>
