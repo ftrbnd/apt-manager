c9ff3e1937bd17be8a934684a87b87f20abf57d4
ftrbnd <giosalas25@gmail.com>
2024-07-19 14:10:44 -0700

    Add hover cards

--- src/components/RentCollection.tsx
+++ src/components/RentCollection.tsx
@@ -9,7 +9,9 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Card, CardContent, CardTitle } from '@/components/ui/card';
 import { useApartments } from '@/hooks/useApartments';
 import Link from 'next/link';
-import { monthNames } from '@/lib/utils';
+import { formatRentChecks, monthNames, toCamelCase } from '@/lib/utils';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card';
+import { Avatar, AvatarFallback } from './ui/avatar';
 
 interface Props {
 	receipts: Receipt[];
@@ -50,19 +52,44 @@ export function RentCollection({ receipts, month, year }: Props) {
 						<AccordionContent>
 							<div className='flex flex-col gap-4'>
 								{apartments.map((apartment) => (
-									<div
-										key={apartment.id}
-										className='flex items-center space-x-2'>
-										<Checkbox
-											id='paid_rent'
-											checked={apartmentPaidRent(apartment)}
-										/>
-										<Link
-											href={`/apartments/${apartment.id}`}
-											className='text-sm font-medium hover:underline'>
-											Apartment #{apartment.number}
-										</Link>
-									</div>
+									<HoverCard key={apartment.id}>
+										<HoverCardTrigger asChild>
+											<div
+												key={apartment.id}
+												className='flex items-center space-x-2'>
+												<Checkbox
+													id='paid_rent'
+													checked={apartmentPaidRent(apartment)}
+												/>
+												<Link
+													href={`/apartments/${apartment.id}`}
+													className='text-sm font-medium hover:underline'>
+													Apartment #{apartment.number}
+												</Link>
+											</div>
+										</HoverCardTrigger>
+										<HoverCardContent className='w-80'>
+											<div className='flex space-x-4'>
+												<Avatar>
+													<AvatarFallback>{apartment.number}</AvatarFallback>
+												</Avatar>
+												<div className='space-y-1'>
+													<h4 className='text-sm font-semibold'>
+														{apartment.tenant}
+													</h4>
+													<p className='text-sm'>
+														Rent: {formatRentChecks(apartment.rent)}
+													</p>
+													<div className='flex items-center pt-2'>
+														<span className='text-xs text-muted-foreground'>
+															Payment method:{' '}
+															{toCamelCase(apartment.paymentMethod)}
+														</span>
+													</div>
+												</div>
+											</div>
+										</HoverCardContent>
+									</HoverCard>
 								))}
 							</div>
 						</AccordionContent>
