a016db098b9c5d6ec907d77563bb25f6e9f677be
ftrbnd <giosalas25@gmail.com>
2024-08-15 11:30:42 -0700

    Update login page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,6 +1,10 @@
 import { Footer } from '@/components/Layout/Footer';
+import { getUser } from '@/lib/auth/actions';
 
-export default function Home() {
+export default async function Home() {
+	const user = await getUser();
+	console.log(user);
+
 	return (
 		<div className='flex p-4 flex-col items-center md:justify-center w-full min-h-screen bg-muted/40'>
 			<div>TODO: USER PROFILE</div>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,11 @@
-export default async function Page() {
+import { Login } from '@/components/Authentiction/Login';
+import { Footer } from '@/components/Layout/Footer';
+
+export default function Page() {
 	return (
-		<>
-			<h1>Sign in</h1>
-			<a href='/login/apple'>Sign in with Apple</a>
-		</>
+		<div className='flex p-4 flex-col items-center md:justify-center w-full min-h-screen bg-muted/40'>
+			<Login />
+			<Footer />
+		</div>
 	);
 }
