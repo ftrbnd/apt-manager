aa7b5b94f0975d84cb3164feeb1a94b4994fbced
ftrbnd <giosalas25@gmail.com>
2024-08-15 11:52:16 -0700

    Create profile form

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,14 +1,29 @@
-import { Footer } from '@/components/Layout/Footer';
-import { getUser } from '@/lib/auth/actions';
+import Link from 'next/link';
+import { ProfileForm } from '@/components/Authentiction/ProfileForm';
 
-export default async function Home() {
-	const user = await getUser();
-	console.log(user);
-
+export default function Dashboard() {
 	return (
-		<div className='flex p-4 flex-col items-center md:justify-center w-full min-h-screen bg-muted/40'>
-			<div>TODO: USER PROFILE</div>
-			<Footer />
+		<div className='flex min-h-screen w-full flex-col'>
+			<main className='flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10'>
+				<div className='mx-auto grid w-full max-w-6xl gap-2'>
+					<h1 className='text-3xl font-semibold'>Account</h1>
+				</div>
+				<div className='mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]'>
+					<nav
+						className='grid gap-4 text-sm text-muted-foreground'
+						x-chunk='dashboard-04-chunk-0'>
+						<Link
+							href='#'
+							className='font-semibold text-primary'>
+							Profile
+						</Link>
+						<Link href='#'>Security</Link>
+					</nav>
+					<div className='grid gap-6'>
+						<ProfileForm />
+					</div>
+				</div>
+			</main>
 		</div>
 	);
 }
---
+++ src/components/Authentiction/ProfileForm.tsx
@@ -0,0 +1,110 @@
+'use client';
+
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { useAuth } from '@/hooks/useAuth';
+import { Save } from 'lucide-react';
+
+const formSchema = z.object({
+	firstName: z.string().trim().min(1, 'Required'),
+	lastName: z.string().trim().min(1, 'Required'),
+	email: z.string().trim().min(1, 'Required'),
+});
+type FormValues = z.infer<typeof formSchema>;
+
+export function ProfileForm() {
+	const { user } = useAuth();
+
+	const form = useForm<FormValues>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			firstName: user?.firstName ?? undefined,
+			lastName: user?.lastName ?? undefined,
+			email: user?.email ?? undefined,
+		},
+	});
+
+	function onSubmit(values: FormValues) {
+		console.log(values);
+	}
+
+	return (
+		<Card x-chunk='dashboard-04-chunk-1'>
+			<CardHeader>
+				<CardTitle>Profile details</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className='space-y-8'>
+						<FormField
+							control={form.control}
+							name='firstName'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>First name</FormLabel>
+									<FormControl>
+										<Input
+											placeholder='John'
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name='lastName'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Last name</FormLabel>
+									<FormControl>
+										<Input
+											placeholder='Doe'
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name='email'
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input
+											placeholder='johndoe@example.com'
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<Button type='submit'>
+							<Save className='w-4 h-4 mr-2' />
+							Save
+						</Button>
+					</form>
+				</Form>
+			</CardContent>
+		</Card>
+	);
+}
