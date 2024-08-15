f368831f4cdfe45f6a0fcd2ac392fb2dca4f1ad2
ftrbnd <giosalas25@gmail.com>
2024-08-15 11:10:01 -0700

    Fix middleware

--- src/lib/auth/actions.ts
+++ src/lib/auth/actions.ts
@@ -1,52 +1,69 @@
 'use server';
 
-import { cookies } from 'next/headers';
-import { cache } from 'react';
-
 import { lucia } from '@/lib/auth/lucia';
 import type { Session, User } from 'lucia';
 import { redirect } from 'next/navigation';
-
-export const validateRequest = cache(
-	async (): Promise<
-		{ user: User; session: Session } | { user: null; session: null }
-	> => {
-		const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
-		if (!sessionId) {
-			return {
-				user: null,
-				session: null,
-			};
-		}
-
-		const result = await lucia.validateSession(sessionId);
-		// next.js throws when you attempt to set cookie when rendering page
-		try {
-			if (result.session && result.session.fresh) {
-				const sessionCookie = lucia.createSessionCookie(result.session.id);
-				cookies().set(
-					sessionCookie.name,
-					sessionCookie.value,
-					sessionCookie.attributes
-				);
-			}
-			if (!result.session) {
-				const sessionCookie = lucia.createBlankSessionCookie();
-				cookies().set(
-					sessionCookie.name,
-					sessionCookie.value,
-					sessionCookie.attributes
-				);
-			}
-		} catch {}
-		return result;
-	}
-);
+import { cookies } from 'next/headers';
 
 export const getUser = async () => {
-	const { user } = await validateRequest();
-
+	const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+	if (!sessionId) return null;
+	const { user, session } = await lucia.validateSession(sessionId);
+	try {
+		if (session && session.fresh) {
+			const sessionCookie = lucia.createSessionCookie(session.id);
+			cookies().set(
+				sessionCookie.name,
+				sessionCookie.value,
+				sessionCookie.attributes
+			);
+		}
+		if (!session) {
+			const sessionCookie = lucia.createBlankSessionCookie();
+			cookies().set(
+				sessionCookie.name,
+				sessionCookie.value,
+				sessionCookie.attributes
+			);
+		}
+	} catch {
+		// Next.js throws error when attempting to set cookies when rendering page
+	}
 	return user;
+};
+
+export const validateRequest = async (): Promise<
+	{ user: User; session: Session } | { user: null; session: null }
+> => {
+	const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+	if (!sessionId) {
+		return {
+			user: null,
+			session: null,
+		};
+	}
+
+	const result = await lucia.validateSession(sessionId);
+	// next.js throws when you attempt to set cookie when rendering page
+	try {
+		if (result.session && result.session.fresh) {
+			const sessionCookie = lucia.createSessionCookie(result.session.id);
+			cookies().set(
+				sessionCookie.name,
+				sessionCookie.value,
+				sessionCookie.attributes
+			);
+		}
+		if (!result.session) {
+			const sessionCookie = lucia.createBlankSessionCookie();
+			cookies().set(
+				sessionCookie.name,
+				sessionCookie.value,
+				sessionCookie.attributes
+			);
+		}
+	} catch {}
+	return result;
 };
 
 export const signOut = async () => {
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,74 @@
 import { verifyRequestOrigin } from 'lucia';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { db } from './lib/drizzle/db';
+import { managers, sessions } from './lib/drizzle/schema';
+import { eq } from 'drizzle-orm';
+import { cookies } from 'next/headers';
+import { lucia } from './lib/auth/lucia';
 
-export async function middleware(request: NextRequest): Promise<NextResponse> {
-	if (request.method === 'GET') {
+export async function middleware(request: NextRequest) {
+	const onApiRoute = request.nextUrl.pathname.startsWith('/api');
+	if (onApiRoute) {
+		if (request.method === 'GET') return NextResponse.next();
+
+		const originHeader = request.headers.get('Origin');
+		const hostHeader = request.headers.get('Host');
+		if (
+			!originHeader ||
+			!hostHeader ||
+			!verifyRequestOrigin(originHeader, [hostHeader, 'appleid.apple.com'])
+		) {
+			return new NextResponse(null, {
+				status: 403,
+			});
+		}
+	}
+
+	const onLoginPage = request.nextUrl.pathname.startsWith('/login');
+	const onOnboardingPage = request.nextUrl.pathname.startsWith('/onboarding');
+	const onAccountPage = request.nextUrl.pathname.startsWith('/account');
+	const onAllowedPage = onOnboardingPage || onAccountPage;
+
+	const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+	if (!sessionId) {
+		if (!onLoginPage) {
+			return NextResponse.redirect(new URL('/login', request.url));
+		}
 		return NextResponse.next();
 	}
-	const originHeader = request.headers.get('Origin');
-	// NOTE: You may need to use `X-Forwarded-Host` instead
-	const hostHeader = request.headers.get('Host');
 
-	if (
-		!originHeader ||
-		!hostHeader ||
-		!verifyRequestOrigin(originHeader, [hostHeader, 'appleid.apple.com'])
-	) {
-		return new NextResponse(null, {
-			status: 403,
-		});
+	const [session] = await db
+		.select()
+		.from(sessions)
+		.where(eq(sessions.id, sessionId));
+
+	if (session.userId) {
+		const [manager] = await db
+			.select()
+			.from(managers)
+			.where(eq(managers.id, session.userId));
+
+		if (!manager) {
+			if (!onAllowedPage) {
+				return NextResponse.redirect(new URL('/onboarding', request.url));
+			}
+
+			return NextResponse.next();
+		} else if (!onLoginPage && !onAllowedPage && !manager.approved) {
+			return NextResponse.redirect(new URL('/onboarding', request.url));
+		} else if (onOnboardingPage && manager.approved) {
+			return NextResponse.redirect(new URL('/', request.url));
+		} else if (onLoginPage) {
+			return NextResponse.redirect(new URL('/', request.url));
+		}
+	} else {
+		if (!onLoginPage || onOnboardingPage) {
+			return NextResponse.redirect(new URL('/login', request.url));
+		}
 	}
-	return NextResponse.next();
 }
+
+export const config = {
+	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
