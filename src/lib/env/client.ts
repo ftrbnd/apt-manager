4713e2a96bead23dec444d81ccde47b4ab444ae6
ftrbnd <giosalas25@gmail.com>
2025-10-02 16:40:00 -0700

    Make redirect uri optional

--- src/lib/env/client.ts
+++ src/lib/env/client.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 export const clientEnv = createEnv({
 	client: {
-		NEXT_PUBLIC_AUTH_REDIRECT_URI: z.string(),
+		NEXT_PUBLIC_AUTH_REDIRECT_URI: z.string().optional(),
 	},
 	runtimeEnv: {
 		NEXT_PUBLIC_AUTH_REDIRECT_URI: process.env.NEXT_PUBLIC_AUTH_REDIRECT_URI,
